b2bf1a4e958d96ae551b5232b814e347c8875a07
zhanlu0729 <ylqi_850729@163.com>
2019-03-30 18:42:26 +0800

    更新配置项

---
+++ eureka-server/src/test/java/spring/cloud/boot/MainTest.java
@@ -0,0 +1,13 @@
+package spring.cloud.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainTest {
+
+    public static void main(String[] args) {
+        args = new String[]{"--spring.application.name=bone-cloud-eureka-server"};
+        Main.run(MainTest.class, args);
+    }
+
+}
